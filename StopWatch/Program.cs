0df77da42edd7ea8854b336ac5f3d9a7bb60ffe7
DanielH <daniel.hosseini1@gmail.com>
2017-08-03 18:05:49 +0200

    first commit

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -6,7 +6,35 @@ namespace StopWatch
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello and welcome to the Startwatch application");
+            Console.WriteLine("Write start to START the stopwatch");
+			Console.WriteLine("Write stop to STOP the stopwatch");
+			Console.WriteLine("Write exit to EXIT the stopwatch");
+
+			var running = true;
+            StopWatch stopWatch = new StopWatch();
+
+
+			while (running)
+            {
+                var input = Console.ReadLine().ToLower();
+                switch (input)
+                {
+                    case "start":
+                        stopWatch.StartCount();
+                        break;
+                    case "stop":
+                        stopWatch.StopCount();
+						break;
+                    case "exit":
+                        Console.WriteLine("The application has ended.");
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input!");
+                        break;
+                }
+            }
         }
     }
 }
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private bool IsRunning { get; set; }
+        private DateTime Start { get; set; }
+        private DateTime Stop { get; set; }
+        private TimeSpan Duration { get;  set; }
+
+
+        public void StartCount()
+        {
+            if (IsRunning)
+                throw new InvalidOperationException("The stopwatch is already running.");
+
+            IsRunning = true;
+            Start = DateTime.Now;
+        }
+
+        public void StopCount()
+        {
+            if (!IsRunning)
+                throw new InvalidOperationException("The stopwatch is not running.");
+
+            IsRunning = false;
+            Stop = DateTime.Now;
+            GetDuration();
+
+        }
+        public void GetDuration(){
+
+            Console.WriteLine("The Stopwatch has been running in total {0}", Duration = Start - Stop);
+        }
+    }
+}
